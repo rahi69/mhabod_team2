8a4d02f16af0fbea58c9f5d9c05b8f190b4ccc0d
FARNIS-PC <farnis.gh@gmail.com>
2018-02-12 15:59:18 +0330

    eroryabi Education

--- admin_panel/Managmant-Gallery.php
+++ admin_panel/Managmant-Gallery.php
@@ -17,11 +17,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
 <body>
-<button class="hidden-lg" id="showListHome" onclick="showListHome()"></button>
-
-<script src="Scripts/main.js"></script>
+<!--<button class="hidden-lg" id="showListHome" onclick="showListHome()"></button>-->
+<!---->
+<!--<script src="Scripts/main.js"></script>-->
 <div>
 
     <img id="imgProfile" src="img/CIRLEpROFILE.jpg"/>
@@ -30,7 +31,9 @@
     <div  id="container" class="col-lg-8">
 
         <div id="header-GAllery" class="row col-lg-12">مدیریت گالری</div>
-
+<div class="row col-xs-12" id="Add-newGallery">
+    <a href="#">اضافه کردن گالری جدید</a>
+</div>
 
         <div class="row col-lg-12 ListGallery">
 
@@ -38,49 +41,8 @@
         </div>
 
     </div>
-    <!--MENU LIST-->
-<!--    <div id="HomeLIST"  class="nav-side-menu col-lg-4">-->
-<!---->
-<!--        <div class="menu-list">-->
-<!---->
-<!--            <ul id="menu-content" class="menu-content collapse out">-->
-<!--                <!--PIC PROFILE-->-->
-<!---->
-<!--                <li data-toggle="collapse" data-target="#ads" class="collapsed">-->
-<!--                    <a class="list" href="index.php"><i class="fa fa-camera fa-lg"></i>صفحه اصلی </a>-->
-<!---->
-<!--                </li>-->
-<!---->
-<!--                <li onclick="showGallery()" data-toggle="collapse" data-target="#ads" class="collapsed">-->
-<!--                    <a class="list" href="Managmant-Gallery.php"><i class="fa fa-camera fa-lg"></i> مدیریت گالری </a>-->
-<!---->
-<!--                </li>-->
-<!---->
-<!---->
-<!--                <li onclick="showArticle()" data-toggle="collapse" data-target="#service" class="collapsed">-->
-<!--                    <a class="list" href="ManagmantArticle.php"><i class="glyphicon glyphicon-duplicate fa-lg"></i> مدیریت مقالات </a>-->
-<!--                </li>-->
-<!---->
-<!---->
-<!--                <li onclick="ShowEducation()" data-toggle="collapse" data-target="#new" class="collapsed">-->
-<!--                    <a class="list" href="Managment-Education.php"><i class="glyphicon glyphicon-briefcase fa-lg"></i> مدیریت آموزش </a>-->
-<!--                </li>-->
-<!--                <li onclick="List()" data-toggle="collapse" data-target="#ads" class="collapsed">-->
 
-
-
-<!--                    <a class="list()" href="#"><i class="fa fa-black-tie fa-lg"></i> درباره من </a>-->
-<!--                </li>-->
-<!---->
-<!---->
-<!--            </ul>-->
-<!---->
-<!--        </div>-->
-<!---->
-<!--    </div>-->
     <div id="HomeLIST"  class="nav-side-menu col-lg-4">
-        <!--BUTTON LIST-->
-        <!--<i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>-->
 
         <?php include TEMPLATE_FRONT . 'nav_right.php'; ?>
 
