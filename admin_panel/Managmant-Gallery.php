8cb5751cfa9eef5a0235fc20ebf12eecb68daee2
masumekarami <masumek83@gmail.com>
2018-02-12 23:10:36 +0330

    gallery and education updated

--- admin_panel/Managmant-Gallery.php
+++ admin_panel/Managmant-Gallery.php
@@ -6,9 +6,9 @@
     <title>مدیریت گالری</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>-->
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>-->
 
     <!--BOOTSTRAP-->
     <link href="Content/bootstrap.css" rel="stylesheet" />
@@ -37,7 +37,7 @@
 
         <div class="row col-lg-12 ListGallery">
 
-  <?php $function->manage_gallery();?>
+  <?php echo $function->manage_gallery();?>
         </div>
 
     </div>
