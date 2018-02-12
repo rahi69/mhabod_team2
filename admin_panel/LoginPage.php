a325783929bac820b7875d9e1106240adac78817
MaryamLotfi <maryam.lotfi.ml@gmail.com>
2018-02-12 10:59:58 +0330

    The Last Login page html

--- admin_panel/LoginPage.php
+++ admin_panel/LoginPage.php
@@ -59,17 +59,17 @@
                     </form>
                 </div>
 
-                <script>
-                    // Get the modal
-                    var modal = document.getElementById('id01');
-
-                    // When the user clicks anywhere outside of the modal, close it
-                    window.onclick = function(event) {
-                        if (event.target == modal) {
-                            modal.style.display = "none";
-                        }
-                    }
-                </script>
+<!--                <script>-->
+<!--                    // Get the modal-->
+<!--                    var modal = document.getElementById('id01');-->
+<!---->
+<!--                    // When the user clicks anywhere outside of the modal, close it-->
+<!--                    window.onclick = function(event) {-->
+<!--                        if (event.target == modal) {-->
+<!--                            modal.style.display = "none";-->
+<!--                        }-->
+<!--                    }-->
+<!--                </script>-->
 
             </div>
         </div>
