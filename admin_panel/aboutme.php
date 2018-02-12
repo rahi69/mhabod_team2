336250c2038dde1c91ec1e94ae32ab6791bf0dab
MaryamLotfi <maryam.lotfi.ml@gmail.com>
2018-02-13 00:03:02 +0330

    Add aboutme.php

--- admin_panel/aboutme.php
+++ admin_panel/aboutme.php
@@ -13,7 +13,7 @@
     <link type="text/css" rel="stylesheet" href="css/materialize.minimum.css"  media="screen,projection"/>
 
     <!-- Compiled and minified JavaScript -->
-<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>-->
+    <script src="js/materialize.minimum.js"></script>
 
 <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquerymin.js"></script>-->
 <!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>-->
